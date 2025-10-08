daf438aea4faa6985daf9ffbd8e5c9215fdc1830
Vanessa Omoragbon <vanessa.omoragbon@outlook.com>
2025-10-08 11:10:32 +0100

    header

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const signupButton = document.querySelector(".to-signup");
 const loginButton = document.querySelector(".to-login");
 const login = document.querySelector(".login");
 const register = document.querySelector(".register");
+const mobilemenuIcon = document.querySelector(".hamburger-menu-icon");
+const mobileMenu = document.querySelector(".mobile-nav-menu");
 
 loginButton.addEventListener("click", () => {
   register.classList.add("hide");
@@ -12,4 +14,9 @@ signupButton.addEventListener("click", () => {
   login.classList.remove("show");
   register.classList.remove("hide");
   console.log("register");
+});
+
+mobilemenuIcon.addEventListener("click", () => {
+  mobileMenu.classList.toggle("display");
+  console.log(mobileMenu);
 });
